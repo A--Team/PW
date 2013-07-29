e34e63fc84c9647f9ed6bb990323e59e0586609e
dave230689 <davidecagna89@gmail.com>
2013-07-29 16:19:31 +0200

    aggiunta visualizzazione pacchetti ricerca

--- search.php
+++ search.php
@@ -9,19 +9,46 @@
   
   $conn = database::dbConnect();  
   if($duration==2)
-    $sql="SELECT * FROM pacchetto WHERE id_utente='0' AND persone='".$npersons."' AND durata<='3'";
+    $sql="SELECT * FROM pacchetto WHERE id_utente='agenzia' AND persone='".$npersons."' AND durata<='3'";
   else if($duration==3)
-    $sql="SELECT * FROM pacchetto WHERE id_utente='0' AND persone='".$npersons."' AND durata>'3'";
+    $sql="SELECT * FROM pacchetto WHERE id_utente='agenzia' AND persone='".$npersons."' AND durata>'3'";
   else
-    $sql="SELECT * FROM pacchetto WHERE id_utente='0' AND persone='".$npersons."'";
+    $sql="SELECT * FROM pacchetto WHERE id_utente='agenzia' AND persone='".$npersons."'";
   $risposta=database::qSelect($conn,$sql);
   $output="";
   while($el=mysql_fetch_array($risposta)){
       $sql_dest="SELECT * FROM destinazione WHERE id='".$el["id_destinazione"]."'";
       $risposta_dest=database::qSelect($conn,$sql_dest);
       $dest=mysql_fetch_array($risposta_dest);
-      if($dest["continente"]==$continent && $dest["citta"]==$city && $dest["tipo"]==$type)
-	$output=$output."<br>".$el["id"];
+      if($dest["continente"]==$continent && $dest["citta"]==$city && $dest["tipo"]==$type){
+	//trasporto
+	$sql_trasp = "SELECT * FROM trasporto WHERE id='".$el["id_trasporto"]."'";
+	$risposta_trasp = database::qSelect($conn, $sql_trasp);
+	$trasp= mysql_fetch_array($risposta_trasp);
+	//pernottamento
+	$sql_pern = "SELECT * FROM pernottamento WHERE id='".$el["id_pernottamento"]."'";
+	$risposta_pern = database::qSelect($conn, $sql_pern);
+	$pern= mysql_fetch_array($risposta_pern);
+
+	//creo il codice html dei pacchetti
+	$costo = $pern["prezzo"]*$el["durata"]*$el["persone"] + $trasp["prezzo"]*$el["persone"];
+	$output=$output.
+	"
+	  <div class='div_viaggio'>
+		  <p class='dest_viaggio'>".$dest["citta"]."
+			  <div class='div_foto_viaggio'>
+				  <img src=./style/images/dest/".$dest["foto"].">
+			  </div> 
+		  </p>
+		  <p>
+		  Viaggio di ".$el["durata"]." notti per ".$el["persone"]." persona/e.<br>
+		  <span class='carat_viaggio'>Partenza il:</span>".$el["data_partenza"].".<br>
+		  <span class='carat_viaggio'>Trasporto:</span>". $trasp["tipo"]."<br>
+		  <span class='carat_viaggio'>Pernottamento:</span>".$pern["tipo"]."<br>
+		  <span class='costo_viaggio'>A soli: ".$costo."€</span>
+		  </p>
+	  </div>";
+      }
   }
   mysql_close($conn);
   if(strlen($output)>0)
