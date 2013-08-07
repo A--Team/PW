794965265a2251b2ea9d4f8ee43d3b070b37ace3
dave230689 <davidecagna89@gmail.com>
2013-08-07 15:47:50 +0200

    pagina index.php finita

---
+++ ajax.js
@@ -0,0 +1,124 @@
+function ajax_request(update_target,page_to_open,post_parameters){
+  if (window.XMLHttpRequest)
+    xmlhttp=new XMLHttpRequest();
+  else
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  xmlhttp.onreadystatechange=function()
+    {
+    if (xmlhttp.readyState==4 && xmlhttp.status==200)
+      {
+	$(update_target).html(xmlhttp.responseText);
+      }
+    } 
+  xmlhttp.open("POST",page_to_open,false);
+  xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xmlhttp.send(post_parameters);
+}
+
+function aggiorna_citta(){
+  var continent=document.getElementById("continent").value;
+  ajax_request("#city","aggiorna_citta.php","continent="+continent);
+  document.getElementById("city").disabled=false;
+}
+
+function aggiorna_citta_pacchetto(){
+  var continent=document.getElementById("continent").value;
+  ajax_request("#city","aggiorna_citta.php","continent="+continent);
+  document.getElementById("city").disabled=false;
+  document.getElementById("pernottamento").innerHTML="";
+  document.getElementById("trasporto").innerHTML="";
+  document.getElementById("attrazioni").innerHTML="";
+  document.getElementById("attrazioni_pacchetto").innerHTML="";
+
+}
+
+function update_packet_options(){
+  var dest_id=document.getElementById("city").value;
+  aggiorna_pernottamento(dest_id);
+  aggiorna_trasporto(dest_id);
+  aggiorna_attrazioni(dest_id);
+}
+
+function aggiorna_pernottamento(dest_id){
+  ajax_request("#pernottamento","aggiorna_pernottamento.php","dest_id="+dest_id);
+}
+
+function aggiorna_trasporto(dest_id){
+  ajax_request("#trasporto","aggiorna_trasporto.php","dest_id="+dest_id);
+}
+
+function aggiorna_attrazioni(dest_id){
+  ajax_request("#attrazioni","aggiorna_attrazioni.php","dest_id="+dest_id);
+}
+
+function aggiungi_attrazione(){
+  id_attr=document.getElementById("attrazioni").value;
+  opzioni = $('#attrazioni_pacchetto option');
+  vettore_attrazioni = $.map(opzioni ,function(opzioni) {
+      return opzioni.value;
+  });
+  vettore_attrazioni=vettore_attrazioni.join(',');
+  if(jQuery.inArray(id_attr, vettore_attrazioni)===-1){
+    ajax_request("#attrazioni_pacchetto","aggiungi_attrazione.php","id_attr="+id_attr+"&"+"vettore_attrazioni="+vettore_attrazioni);
+  }
+}
+
+function rimuovi_attrazione(){
+  id_attr=document.getElementById("attrazioni_pacchetto").value;
+  opzioni = $('#attrazioni_pacchetto option');
+  vettore_attrazioni = $.map(opzioni ,function(opzioni) {
+      return opzioni.value;
+  });
+  vettore_attrazioni=vettore_attrazioni.join(',');
+  if(id_attr.length>0)
+    ajax_request("#attrazioni_pacchetto","rimuovi_attrazione.php","id_attr="+id_attr+"&"+"vettore_attrazioni="+vettore_attrazioni);
+}
+
+function controlla_form(){
+  city=document.getElementById("city").value;
+  npersons=document.getElementById("npersons").value;
+  duration=document.getElementById("duration").value;
+  datepicker=document.getElementById("datepicker").value;
+  pernottamento=document.getElementById("pernottamento").value;
+  trasporto=document.getElementById("trasporto").value;
+  id_attr=document.getElementById("attrazioni_pacchetto").value;
+  opzioni = $('#attrazioni_pacchetto option');
+  vettore_attrazioni = $.map(opzioni ,function(opzioni) {
+      return opzioni.value;
+  });
+  vettore_attrazioni=vettore_attrazioni.join(',');
+  if(city.length>0 && datepicker.length>0 && pernottamento.length>0 && trasporto.length>0){
+    post_string="city="+city+"&"+"duration="+duration+"&"+"npersons="+npersons+"&"+"datepicker="+datepicker+"&"+"pernottamento="+pernottamento+"&"+"trasporto="+trasporto+"&"+"vettore_attrazioni="+vettore_attrazioni;
+    $("#err_content").html("");
+    ajax_request("#confirm_content","crea_pacchetto.php",post_string);
+    document.getElementById("city").innerHTML="";
+    document.getElementById("city").disabled=true;
+    document.getElementById("datepicker").value="";
+    document.getElementById("pernottamento").innerHTML="";
+    document.getElementById("trasporto").innerHTML="";
+    document.getElementById("attrazioni").innerHTML="";
+    document.getElementById("attrazioni_pacchetto").innerHTML="";
+  }
+  else{
+    $("#confirm_content").html("");
+    $("#err_content").html("Attenzione, riempire tutti i campi.");
+  }
+}
+
+function search(){
+  var xmlhttp;
+  var continent=document.getElementById("continent").value;
+  var city=document.getElementById("city").value;
+  var type=document.getElementById("type").value;
+  var duration=document.getElementById("duration").value;
+  var npersons=document.getElementById("npersons").value;
+  var data_partenza1=document.getElementById("datepicker1").value;
+  var data_partenza2=document.getElementById("datepicker2").value;
+  if(city.length>0 && data_partenza1.length>0 && data_partenza2.length>0){
+    post_string="continent="+continent+"&"+"city="+city+"&"+"type="+type+"&"+"duration="+duration+"&"+"npersons="+npersons+"&"+"data_partenza1="+data_partenza1+"&"+"data_partenza2="+data_partenza2;
+    ajax_request("#content","search.php",post_string);
+    $("#err_content").html("");
+  }
+  else
+    $("#err_content").html("Attenzione, non hai selezionato la città o il range della data di partenza.");
+}
--- index.php
+++ index.php
@@ -5,6 +5,27 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="description" content="">
     <link rel="stylesheet" type="text/css" href="style/style.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script type="text/javascript" src="ajax.js"></script>
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+    <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+    <script>
+    $(function(){
+      $("#datepicker1").datepicker({ dateFormat: 'yy-mm-dd' }).val();
+      $("#datepicker2").datepicker({ dateFormat: 'yy-mm-dd' }).val();
+    });
+    </script>
+    <script>
+    function minus(){
+      if($('#duration').val()>1)
+	$('#duration').val(parseInt($('#duration').val())-1);
+    }
+    function plus(){
+      if($('#duration').val()<20)
+	$('#duration').val(parseInt($('#duration').val())+1);
+    }
+    </script>
   </head>
   <body>
     <div id="container">
@@ -12,9 +33,9 @@
 	<div id="logo">
 			<span class="ribbon">
 				<a href="index.php"><span>Home</span></a>
-				<a href="#"><span>Catalogo</span></a>
-				<a href="#"><span>Pippo</span></a>
-				<a href="#"><span>Pluto</span></a>
+				<a href="catologo.php"><span>Catalogo</span></a>
+				<a href="dovesiamo.php"><span>Dove siamo</span></a>
+				<a href="contatti.php"><span>Contatti</span></a>
 			</span>			
 	</div>
 	<div id="login">
@@ -24,16 +45,13 @@
 			include_once 'config.php';
 			//controllo esistenza della sessione
 			if(isset($_SESSION[$session_name])){
-				//attivo la sessione
-				//$_SESSION[$session_name] = true;
 				echo "<br><br><h2>Benvenuto<a href='homepersonale.php'> " . $_SESSION['username'] . "!</a></h2><br>";
 				echo "<form method='POST' action='logout.php'>
 						<input type='submit' value='logout' class='btn_login'>
 					  </form>";
 			}
-			else
-			{
-					include 'login_form.php';
+			else{
+				include 'login_form.php';
 			}
 		?>
 	</div>
@@ -42,16 +60,90 @@
 	<div id="content">
 		<?php
 			include 'pacchetto.php';
-			$pacchetti=new pacchetto('home',[]);
+			$pacchetti=new pacchetto('home');
 			$pacchetti->stampa();
 		?>
 	</div>
 	<div id="navigation">
-	  	<div>colonna laterale</div>
+	  <br>
+	  <div class="search_space">
+	  <h3>Cerca il tuo viaggio:</h3>
+	  <br>
+	  <table>
+	  <tr>
+	  <td>Continente:</td>
+	  <td>
+	    <select id="continent" onchange='aggiorna_citta()'>
+	      <option value="" disabled selected>Seleziona</option>
+	      <option value="africa">Africa</option>
+	      <option value="america">America</option>
+	      <option value="asia">Asia</option>
+	      <option value="europa">Europa</option>
+	      <option value="oceania">Oceania</option>
+	    </select>
+	  </td>
+	  </tr>
+	  <tr>
+	  <td>Città:</td>
+	  <td>
+	    <select id="city" disabled>
+	    </select>
+	  </td>
+	  </tr>
+	  <tr>
+	  <td>Tipologia:</td>
+	  <td>
+	    <select id="type">
+	      <option value="culturale">Culturale</option>
+	      <option value="relax">Relax</option>
+	      <option value="divertimento">Divertimento</option>
+	    </select>
+	  </td>
+	  </tr>
+	  <tr>
+	  <td>Durata max:</td>
+	  <td>
+	  <button type="button" onclick="minus()">-</button><input type="text" id="duration" value="1" class="duration_field" readonly><button type="button" onclick="plus()">+</button>
+	  </td>
+	  </tr>
+	  <tr>
+	  <td>N. persone:</td>
+	  <td>
+	    <select id="npersons">
+	    <option value="1">1</option>
+	    <option value="2">2</option>
+	    <option value="3">3</option>
+	    <option value="4">4</option>
+	    <option value="6">6</option>
+	  </select>
+	  </td>
+	  </tr>
+	  </table>
+	  <h5>Quando vuoi viaggiare?</h5>
+	  <table>
+	  <tr>
+	    <td>
+	      Dal:
+	    </td>
+	    <td>
+	      <input type="text" id="datepicker1" name="data_partenza1" class="date_input"/>
+	    </td>
+	  </tr>
+	  <tr>
+	    <td>
+	      Al:
+	    </td>
+	    <td>
+	      <input type="text" id="datepicker2" name="data_partenza2" class="date_input"/>
+	    </td>
+	  </tr>
+	  </table>
+	  <button type="button" onclick='search()'>Cerca</button>
+	  <div id="err_content"></div></div>
 	</div>
       </div> 
       <div id="footer">
-		<div>footer</div>
+	<div>footer</div>
       </div>
     </div>
   </body>
