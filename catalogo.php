50be7712546eeffcb7b553f812eb5fe244858ced
GianBur <gian_bur@hotmail.it>
2013-08-16 12:34:09 +0200

    catalogo

---
+++ catalogo.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+  <head>
+    <title>Agenzia Viaggi</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="description" content="">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+  </head>
+  <body>
+    <div id="container">
+    <div id="header_container">
+	<div id="logo">
+			<span class="ribbon">
+				<a href="index.php"><span>Home</span></a>
+				<a href="catalogo.php"><span>Catalogo</span></a>
+				<a href="dovesiamo.php"><span>Dove siamo</span></a>
+				<a href="contatti.php"><span>Contatti</span></a>
+			</span>			
+	</div>
+	<div id="login">
+		<?php
+			session_start();
+			include 'login_form.php';
+		?>
+	</div>
+	</div>
+      <div id="content_container">
+	<div id="content">
+		<?php
+			include 'pacchetto.php';
+			$pacchetti=new pacchetto('catalogo',[]);
+			$pacchetti->stampa();
+		?>
+	</div>
+	<div id="navigation">
+    	<?php
+	  		include_once 'config.php';
+			if(isset($_SESSION[$session_name]))
+			{
+		?> 
+                <br>
+                <div id='vert_menu'>		
+                       <a href='homepersonale.php' id="selected"><span>La mia home</span></a>	
+                       <a href='ordini.php'><span>Le mie prenotazioni</span></a>
+                       <a href='wish_list.php'><span>I miei desideri</span></a>
+                       <a href='storico.php'><span>I miei viaggi</span></a>
+                       <a href='gestioneprofilo.php'><span>Il mio profilo</span></a>			
+                </div>
+       <?php
+			}
+		?>
+	</div>
+      </div> 
+      <div id="footer">
+	<div>footer</div>
+      </div>
+    </div>
+  </body>
+</html>
+ 
