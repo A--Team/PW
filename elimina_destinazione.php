1e058f87c1f3dd39c7ea9740eb995c05ab330e3e
dave230689 <davidecagna89@gmail.com>
2013-09-08 14:41:27 +0200

    Delete elimina_destinazione.php

--- elimina_destinazione.php
+++
@@ -1,8 +0,0 @@
-<?php
-  include_once 'database.php';
-  $dest_id=$_POST['dest_id'];
-  $conn = database::dbConnect();
-  $sql="UPDATE destinazione SET visible='FALSE' WHERE id='".$dest_id."'";
-  database::qUpdate($conn,$sql);
-  mysql_close($conn);
-?>    
