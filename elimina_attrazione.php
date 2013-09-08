e2c832dc5f72a8f79d0385a1d4836f17828d33a6
dave230689 <davidecagna89@gmail.com>
2013-09-08 14:41:21 +0200

    Delete elimina_attrazione.php

--- elimina_attrazione.php
+++
@@ -1,8 +0,0 @@
-<?php
-  include_once 'database.php';
-  $attr_id=$_POST['attr_id'];
-  $conn = database::dbConnect();
-  $sql="UPDATE attrazioni SET visible='FALSE' WHERE id='".$attr_id."'";
-  database::qUpdate($conn,$sql);
-  mysql_close($conn);
-?>   
