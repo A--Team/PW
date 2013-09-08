aa91aff309194e4ba628c220480ea2c40e7d73ef
dave230689 <davidecagna89@gmail.com>
2013-09-08 14:41:43 +0200

    Delete elimina_trasporto.php

--- elimina_trasporto.php
+++
@@ -1,7 +0,0 @@
-<?php
-  include_once 'database.php';
-  $trasp_id=$_POST['trasp_id'];
-  $conn = database::dbConnect();
-  $sql="UPDATE trasporto SET visible='FALSE' WHERE id='".$trasp_id."'";
-  database::qUpdate($conn,$sql);
-?>   
