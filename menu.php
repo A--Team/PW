87d747883035876b7fc214a42d578cb4b85a14e6
dave230689 <davidecagna89@gmail.com>
2013-09-08 14:42:25 +0200

    Delete menu.php

--- menu.php
+++
@@ -1,8 +0,0 @@
-<?php
-   echo  '<span class="ribbon">
-	    <a href="index.php"><span>Home</span></a>
-	    <a href="catalogo.php"><span>Catalogo</span></a>
-	    <a href="dovesiamo.php"><span>Dove siamo</span></a>
-	    <a href="contatti.php"><span>Contatti</span></a>
-	  </span>';
-?>
