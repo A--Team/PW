72c9d588bf856b847cdc857137c6239ee26b5e31
dave230689 <davidecagna89@gmail.com>
2013-09-08 14:41:35 +0200

    Delete elimina_pernottamento.php

--- elimina_pernottamento.php
+++
@@ -1,8 +0,0 @@
-<?php
-  include_once 'database.php';
-  $pern_id=$_POST['pern_id'];
-  $conn = database::dbConnect();
-  $sql="UPDATE pernottamento SET visible='FALSE' WHERE id='".$pern_id."'";
-  database::qUpdate($conn,$sql);
-?>   
- 
