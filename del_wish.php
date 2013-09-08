d8387c17c646656c4c6f76da1999dc03d7add3fe
dave230689 <davidecagna89@gmail.com>
2013-09-08 14:41:13 +0200

    Delete del_wish.php

--- del_wish.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include_once 'database.php';
-    $id = $_POST['id'];
-	$conn=database::dbConnect();
-	$sql = "DELETE FROM `pacchetto` WHERE `id` = $id";
-	database::qDelete($conn,$sql);
-	database::dbClose();
-?>
